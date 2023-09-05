c8e42151f9ae5e83b6b8c80496cbd71793d0f903
messi618 <54788160+messi618@users.noreply.github.com>
2023-09-05 15:06:30 -0400

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,10 @@ const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>Portal</h1>
+            <div className="links">
+                <a href="">all companies</a>
+                <a href="">create</a>
+            </div>
         </nav>
      );
 }
